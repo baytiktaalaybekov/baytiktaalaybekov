db55bac73bc89b62c3b331615dfc53ae58b14859
baytiktaalaybekov <baytiktaalaybekov@gmail.com>
2022-10-25 14:21:19 +0600

    jklhlkj

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] mas = {1, 5, 653, 21, 23, 32};
+        System.out.println(Arrays.toString(method(mas)));
+
+
+    }
+
+    public static int[] method(int[] array) {
+        Arrays.sort(array);
+        int[] newArr = new int[array.length];
+        for (int i = 0; i < array.length - array.length; i++) {
+
+
+            newArr[i] = array[i];
+
+        }
+        return newArr;
+
+    }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
